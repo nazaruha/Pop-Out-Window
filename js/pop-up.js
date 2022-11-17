539cb46b53d752a4dd49faeada3d414678d74252
Nazar <fedunnazar20122001@gmail.com>
2022-11-18 00:21:00 +0200

    somehow is done

---
+++ js/pop-up.js
@@ -0,0 +1,18 @@
+﻿const popUp_window = document.getElementById("registration-form");
+const TitleWindow = document.getElementById("title");
+const btnYes = document.getElementById("btnYes");
+const btnNo = document.getElementById("btnNo");
+const btnClose = document.getElementById("btnTitleClose");
+
+btnYes.addEventListener("click", function () {
+    popUp_window.style.display = "block";
+});
+
+btnNo.addEventListener("click", function () {
+    btnNo.style.display = "none";
+});
+
+btnClose.addEventListener("click", function () {
+    popUp_window.style.display = "none";
+    TitleWindow.style.display = "block";
+});
